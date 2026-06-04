2fce7f0a3fb31b278aff6ce306a3c17ac51f50dc
marvinsakali <marvinsakali09@gmail.com>
2026-06-04 18:38:19 +0300

    modified index.js

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ let messageEL = document.getElementById('message')
 let cardEl = document.getElementById('cards')
 let sumEl = document.querySelector('#sum')
 
-function startGame() {
+
+function startGame(){
+    renderGame()
+}
+
+function renderGame() {
     cardEl.textContent = " "
-    for (let i = 0; i < 5 ; i++){
+    for (let i = 0; i < cards.length ; i++){
         cardEl.textContent += cards[i] + " ♦ "
     }
     // cardEl.textContent = firstCard + " ♦ "  + secondCard + " ♠"
@@ -28,19 +33,9 @@ function startGame() {
 }
 
 function newCard(){
-    let nextCard = 6
-    sum += nextCard
-    cards.push(nextCard)
-    sumEl.textContent = sum
-    if (sum < 21){
-        let message = "Try another number"
-        messageEL.textContent = message
-        
-    } else if (sum === 21){
-        let message = "Congrats you won"
-        messageEL.textContent = message
-    } else {
-        let message = "You are out of the game!"
-        messageEL.textContent = message
-    }
+    let card = 6
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
 }
